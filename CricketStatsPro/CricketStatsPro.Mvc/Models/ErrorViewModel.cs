1069be191b56734edc4bfad7a0b4fa1bcba36421
Thomas Gonzalez Miranda <thomasgm@gmail.com>
2026-04-29 21:50:33 -0300

    Inicio Projeto 2

---
+++ CricketStatsPro/CricketStatsPro.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CricketStatsPro.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ CricketStatsPro/CricketStatsPro.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CricketStatsPro.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
